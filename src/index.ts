62dc2b61148b6a6c557ce46ea93e3747aa12155a
J-RAP <srrapero720@hotmail.com>
2026-08-30 03:53:07 -0600

    chore: update dependencies and typescript config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Page } from 'playwright-core';
+import type { Page } from 'playwright-core';
 import axios from 'axios';
 
 const rnd = (max: number, min: number): number => Math.floor(Math.random() * (max - min)) + min;
